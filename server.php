3ac4325c4246a7704a500e538c99cc903f27025c
Muhammad Syifa <emsifa@gmail.com>
2020-05-12 12:16:00 +0700

    fix mimetype for .wasm file in PHP built-in server

--- server.php
+++ server.php
@@ -15,7 +15,7 @@
 
 $path = pathinfo($_SERVER["SCRIPT_FILENAME"]);
 if ($path["extension"] == "wasm") {
-    header("Content-Type: text/x-script.elisp");
+    header("Content-Type: application/wasm");
     readfile($_SERVER["SCRIPT_FILENAME"]);
 } else {
     return FALSE;

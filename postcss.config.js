510bb04afb4f65011b45e679f4717458dd387aee
Muhammad Syifa <emsifa@gmail.com>
2020-05-12 12:15:43 +0700

    whitelist body and html style from purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,10 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: [
     './src/*.rs',
+    './static/*.html',
   ],
+
+  whitelist: ['html', 'body'],
 
   // This is the function used to extract class names from your templates
   defaultExtractor: content => {
